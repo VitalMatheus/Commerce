6ba4c1dace384ce9fee953e44af649c35acc6fc5
Matheus Vital Diniz de Souza <matheus@localhost.localdomain>
2022-09-09 10:45:04 -0300

    Feat: Adiciona requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,6 @@
+// use("commerce")
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $set: { ultimaModificacao: Date } },
+);
+db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { nome: 1, _id: 0 });
